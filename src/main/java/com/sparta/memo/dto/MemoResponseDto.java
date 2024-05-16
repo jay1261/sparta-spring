d5acc779c85d7d11bf894c62735334a074cb8ea4
jay1261 <dongjae73@naver.com>
2024-05-16 11:01:31 +0900

    create, read

---
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -0,0 +1,17 @@
+package com.sparta.memo.dto;
+
+import com.sparta.memo.entity.Memo;
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String userName;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.userName = memo.getUserName();
+        this.contents = memo.getContents();
+    }
+}
