f2cfe3d7b7de396145e67e6d9602ea944e23b36c
jay1261 <dongjae73@naver.com>
2024-05-23 21:08:55 +0900

    @OneToOne 양방향 및 조회

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,4 +13,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) {
+        this.food = food;
+        food.setUser(this);
+    }
+
 }
