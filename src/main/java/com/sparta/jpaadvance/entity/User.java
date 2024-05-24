136239249cf5eebe26b71eb86eaf9b181fdec995
jay1261 <dongjae73@naver.com>
2024-05-24 16:32:05 +0900

    orphanRemoval 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,7 +17,9 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}) // 영속성 전이
+
+    // 고아 entity 삭제 (orphanRemoval)는 Cascade Remove도 동작한다. 이를 쓸 때에는 연관된 부분에 문제가 없는지 확인을 잘 해야함.
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST}, orphanRemoval = true)
     private List<Food> foodList = new ArrayList<>();
 
     public void addFoodList(Food food) {
