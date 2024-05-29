876c8fd5ed5f078b1f69998912c5f083f5fe2ce4
jay1261 <dongjae73@naver.com>
2024-05-29 17:35:22 +0900

    validation 예외처리

--- spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -4,13 +4,18 @@ import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.parameters.P;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
@@ -32,21 +37,19 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        // 예외가 발생하면 BindingResult 객체에 오류에 대한 정보가 담겨서 들어온다.
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
     }
-
-//    @PostMapping("/user/login")
-//    public String login(LoginRequestDto requestDto, HttpServletResponse res){
-//
-//        try {
-//            userService.login(requestDto, res);
-//        } catch (Exception e) {
-//            return "redirect:/api/user/login-page?error";
-//        }
-//
-//        return "redirect:/";
-//    }
 }
