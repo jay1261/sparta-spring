b50c95a2cba30212097e78c3ff6c257514592d75
jay1261 <dongjae73@naver.com>
2024-05-29 16:02:56 +0900

    Spring security 로그인

---
+++ spring-auth/src/main/java/com/sparta/springauth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sparta.springauth.security;
+
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// UserDetailsServiceImpl, UserDetailsImpl 를 만들면 시큐리티 기본 로그인을 사용하지 않는다.
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
