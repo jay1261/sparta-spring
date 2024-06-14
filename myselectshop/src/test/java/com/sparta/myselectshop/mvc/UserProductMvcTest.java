ad708fec1d9b74688766838b7070525bf4777cfd
jay1261 <dongjae73@naver.com>
2024-06-14 16:46:39 +0900

    controller 테스트

---
+++ myselectshop/src/test/java/com/sparta/myselectshop/mvc/UserProductMvcTest.java
@@ -0,0 +1,154 @@
+package com.sparta.myselectshop.mvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.myselectshop.config.WebSecurityConfig;
+import com.sparta.myselectshop.controller.ProductController;
+import com.sparta.myselectshop.controller.UserController;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.FolderService;
+import com.sparta.myselectshop.service.KakaoService;
+import com.sparta.myselectshop.service.ProductService;
+import com.sparta.myselectshop.service.UserService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+// WebMvcTest를 통해서 컨트롤러 테스트 가능
+@WebMvcTest(
+        // 테스트할 컨트롤러 지정
+        controllers = {UserController.class, ProductController.class},
+        // 제외할 필터 지정
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = WebSecurityConfig.class
+                )
+        }
+)
+
+class UserProductMvcTest {
+    // MockMvc 사용
+    private MockMvc mvc;
+    // 가짜 인증을 위한 Principal 필요
+    private Principal mockPrincipal;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean // 빈 주입이 필요해서 가짜 빈을 만들어준다.
+    UserService userService;
+
+    @MockBean
+    KakaoService kakaoService;
+
+    @MockBean
+    ProductService productService;
+
+    @MockBean
+    FolderService folderService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter())) // 테스트용으로 만든 가짜 필터를 넣어줌
+                .build();
+    }
+
+    private void mockUserSetup() {
+        // 컨트롤러에서 @AuthenticationPrincipal에서 유저를 뽑아오기 위해 가짜 유저를 만들어서 Principal에  넣어준다
+        // Mock 테스트 유져 생성
+        String username = "sollertia4351";
+        String password = "robbie1234";
+        String email = "sollertia@sparta.com";
+        UserRoleEnum role = UserRoleEnum.USER;
+        User testUser = new User(username, password, email, role);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+    }
+
+    @Test
+    @DisplayName("로그인 Page")
+    void test1() throws Exception {
+        // when - then
+        mvc.perform(get("/api/user/login-page"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원 가입 요청 처리")
+    void test2() throws Exception {
+        // given
+        MultiValueMap<String, String> signupRequestForm = new LinkedMultiValueMap<>();
+        signupRequestForm.add("username", "sollertia4351");
+        signupRequestForm.add("password", "robbie1234");
+        signupRequestForm.add("email", "sollertia@sparta.com");
+        signupRequestForm.add("admin", "false");
+
+        // when - then
+        mvc.perform(post("/api/user/signup")
+                        .params(signupRequestForm)
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/api/user/login-page"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("신규 관심상품 등록")
+    void test3() throws Exception {
+        // given
+        this.mockUserSetup();
+        String title = "Apple <b>아이폰</b> 14 프로 256GB [자급제]";
+        String imageUrl = "https://shopping-phinf.pstatic.net/main_3456175/34561756621.20220929142551.jpg";
+        String linkUrl = "https://search.shopping.naver.com/gate.nhn?id=34561756621";
+        int lPrice = 959000;
+        ProductRequestDto requestDto = new ProductRequestDto(
+                title,
+                imageUrl,
+                linkUrl,
+                lPrice
+        );
+
+        String postInfo = objectMapper.writeValueAsString(requestDto);
+
+        // when - then
+        mvc.perform(post("/api/products")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
